2359934b16028f2d4706a40f164b058d98cb8f0a
R-oso <luukroso@gmail.com>
2022-04-11 14:24:26 +0200

    Added different includes, a .gitignore and succes.php.

---
+++ succes.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+require_once 'includes/functions.php';
+require_once 'includes/database.php';
+/** @var mysqli $db */
+
+if(isset($_SESSION["loggedIn"]) == 0 && isset($_SESSION["userName"]) == 0){
+    header("location: login.php?error=notloggedin");
+    die();
+}
+
+// if(!isset($_SESSION["loggedIn"]) && !isset($_SESSION["userName"])){
+//     header("location: login.php?error=notloggedin");
+//     die();
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js" defer></script>  
+    <script src="https://cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js" defer></script>
+
+    <title>Teachable Machine</title>
+</head>
+
+<body>
+    <h1>Je bent ingelogd</h1>
+    <button type='button' onclick='init()'>Start</button>
+    <div id="webcam-container"></div>
+    <div id="label-container"></div>
+
+    <script src="teachableMachine.js"></script>
+</body>
+
+</html>
